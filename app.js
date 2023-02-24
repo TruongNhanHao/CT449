b986dcc783bfb74a74a929d8c5ed1541c33e0bfb
HaoNhan <truongnhanhao107@gmail.com>
2023-02-24 11:28:56 +0700

    three commit

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const port = 3000;
+const ApiError = require("./app/api-error.js");
 
-app.use(cors());
+const app = express();
+const contactsRouter = require("./app/routes/contact.route");
+
 app.use(express.json());
+app.use(cors());
+
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to contact book application" });
+  res.send({ message: "Welcome to contact book application." });
 });
+
+app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
 app.use((req, res, next) => {
   return next(new ApiError(404, "Resource not found"));
 });
-app.use((err, req, res, next) => {
+
+app.use((error, req, res, next) => {
   return res.status(error.statusCode || 500).json({
     message: error.message || "Internal Server Error",
   });
 });
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
